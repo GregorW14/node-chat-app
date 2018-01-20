b03b26b43cc0837bce1d09f26a1adc310d8b4d72
GregorW14 <gregwhyte14@gmail.com>
2018-01-20 18:51:27 +0000

    Add users class and update user list

---
+++ server/utils/users.js
@@ -0,0 +1,55 @@
+[{
+  id: '4yr792yr9r9y98dwf9w0',
+  name: 'Gregor',
+  room: 'Room 1'
+}]
+
+//addUser(id, name, room)
+
+//RemoveUser(id)
+
+//getUser(id)
+
+//getUserList(room)
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.users.filter((user) => user.id === id)[0];
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
+
+// class Person {
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} year(s) old.`;
+//   }
+// }
+//
+// var me = new Person('Gregor', 22);
+//var description = me.getUserDescription();
